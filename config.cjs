5979b2a871ad0dbe6f4dc6e36fc01e622c90d5db
silencewilson <silence13wilson@gmail.com>
2025-05-12 11:13:01 +0200

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -31,8 +31,8 @@ const config = {
   MODE: process.env.MODE || "public",
   
   // Alliance Info
-  OWNER_NAME: process.env.OWNER_NAME || "Frank kaumba",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "265993702468",
+  OWNER_NAME: process.env.OWNER_NAME || "WE THE BEST",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "263716306870,
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyA3-FskH71WtIQbzrhMA7WAC4Th2zqSNiE",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
